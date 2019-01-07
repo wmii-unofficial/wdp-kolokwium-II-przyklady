b403421068996213881eb8babf0c708b7e6c1571
crumped <46249250+crumped@users.noreply.github.com>
2019-01-07 12:38:51 +0100

    Create cyfra-litera.c
    zadanie 2 z strony 4 zadaniaNaKolokwium2

---
+++ cyfra-litera.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+int czyHaslo(char []);
+int czyWielka(char );
+int czyCyfra(char );
+
+int main()
+{
+    char tab[10];
+    gets(tab);
+    if(czyHaslo(tab))
+    {
+        printf("ok");
+        return 1;
+    }
+    else
+    {
+        printf("zle jak....");
+    }
+
+    return 0;
+}
+int czyHaslo(char haslo[])
+{
+    int i=0,ileZnakow=0;
+     while(haslo[i]!='\0')i++;
+        ileZnakow=i;
+    if (ileZnakow!=9)return 0;
+    if(!(czyCyfra(haslo[0])))return 0;
+    i=1;
+    while(haslo[i]!='\0')
+    {
+        if(!(czyWielka(haslo[i])))return 0;
+        i++;
+    }
+    return 1;
+
+}
+int czyWielka(char znak)
+{
+    if (znak>='A' && znak<='Z')return 1;
+    return 0;
+}
+int czyCyfra(char znak)
+{
+    if (znak>='0' && znak<='9')return 1;
+    return 0;
+}
+
+//if(haslo[0] jest cyfrą?)
+//if(haslo[1]-haslo[5?] to litery)
+//if(haslo[lokalizacja myslnika] to myslnik)?
